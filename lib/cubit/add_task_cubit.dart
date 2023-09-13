f189c668fa5eaaac8c9a42ef1349c708120e2eed
AbdulwahabJ <67449655+AbdulwahabJ@users.noreply.github.com>
2023-09-13 09:05:44 +0300

    add taske methode to cubit

--- lib/cubit/add_task_cubit.dart
+++ lib/cubit/add_task_cubit.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:tasks_app/constant.dart';
 
 import '../models/task_model.dart';
 
 part 'add_task_state.dart';
 
-class AddTaskCubit extends Cubit<AddTaskState>{
+class AddTaskCubit extends Cubit<AddTaskState> {
   AddTaskCubit() : super(AddTaskInitial());
 
-  addTask(TaskModel task){
-    
+  addTask(TaskModel task) async {
+    //
+    emit(AddTaskLoding());
+    //
+    try {
+      var taskBox = Hive.box(kTaskBox);
+      emit(AddTaskSuccess());
+
+      await taskBox.add(task);
+      //
+    } on Exception catch (e) {
+      emit(AddTaskFailure(e.toString()));
+    }
   }
 }
